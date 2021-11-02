7df0efd2a4c94818e7ae2dbb5053c073027e1da4
Ragini Patel <raginip@tekion.com>
2021-11-02 12:40:07 +0530

    app properties

--- Cricket/src/main/java/com/demo/cricket/entities/Player.java
+++ Cricket/src/main/java/com/demo/cricket/entities/Player.java
@@ -1,12 +1,13 @@
 package com.demo.cricket.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document(collection  = "player")
 public class Player {
     @Id
     public String id;
-    public String name;
+    public String firstName;
+    public String lastName;
 
-    public Player() {
-    }
 }
